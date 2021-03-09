0623b39f5c09a011cbd8b637e41c6b494c09580b
ihectorlujan96 <ihectorlujan96@gmail.com>
2021-03-08 18:18:42 -0600

    vistas moviles de administrador

--- src/keys.js
+++ src/keys.js
@@ -3,7 +3,7 @@ module.exports = {
     database: {
         host: 'localhost',
         user: 'root',
-        password: 'password',
+        password: 'silver',
         database: 'residenciap_depi',
 
     }
